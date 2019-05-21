57715d4add73073111941a299f7eddd0174e7177
Ron Heywood <ron.heywood@spektrix.com>
2019-05-21 11:44:02 +0100

    Adding a joke to the Enchanted Maze Room

--- MazeGame/MazeElements/EnchantedMaze/EnchantedRoom.cs
+++ MazeGame/MazeElements/EnchantedMaze/EnchantedRoom.cs
@@ -5,7 +5,7 @@
         public override string ToString()
         {
             var room = string.Join("\r\n", _sides);
-            return $"You are in a Magical Realm. You see\r\n{room}.\r\n{_exits}";
+            return $"You are in a Magical Realm. You see\r\n{room}.\r\n{_exits}\r\n\r\nThorin sits down and starts singing about gold.";
         }
     }
 }
